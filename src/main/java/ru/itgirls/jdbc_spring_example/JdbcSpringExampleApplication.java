7ab1527572cb08c09d1778401137b096641c18f4
DianaGL96 <gorina.diana1996@gmail.com>
2024-07-04 14:15:48 +0300

    Initial commit

---
+++ src/main/java/ru/itgirls/jdbc_spring_example/JdbcSpringExampleApplication.java
@@ -0,0 +1,11 @@
+package ru.itgirls.jdbc_spring_example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcSpringExampleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcSpringExampleApplication.class, args);
+	}
+
+}
